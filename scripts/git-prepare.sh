1e0b56f8847837c6794d4ce4c9b49a0bcd9759b2
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-11-17 10:04:50 -0500

    website: change condition name to label

---
+++ scripts/git-prepare.sh
@@ -0,0 +1,11 @@
+cd docs/threshold/threshold/psychojs
+git checkout threshold-prod # psychojs
+npm i
+cd ..
+git checkout main # threshold
+npm i
+cd ..
+git checkout main # threshold-scientist
+npm i
+cd ..
+git checkout main # website
---
+++ scripts/git-pull.sh
@@ -0,0 +1,8 @@
+cd docs/threshold/threshold/psychojs
+git pull --rebase # psychojs
+cd ..
+git pull --rebase # threshold
+cd ..
+git pull --rebase # threshold-scientist
+cd ..
+git pull --rebase # website
