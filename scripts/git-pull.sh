a36c05dcf1c78e6806a4ba14b2f2b1c30bcc254f
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-11-17 18:08:45 -0500

    website: correct file paths

--- scripts/git-pull.sh
+++ scripts/git-pull.sh
@@ -1,8 +1,21 @@
+#!/bin/sh
+
+DEFAULT=$(tput setaf 7)
+YELLOW=$(tput setaf 3)
+
 cd docs/threshold/threshold/psychojs
+
+echo "${YELLOW}Pulling psychojs${DEFAULT}"
 git pull --rebase # psychojs
+
 cd ..
+echo "${YELLOW}Pulling threshold${DEFAULT}"
 git pull --rebase # threshold
+
 cd ..
+echo "${YELLOW}Pulling threshold-scientist${DEFAULT}"
 git pull --rebase # threshold-scientist
+
 cd ..
+echo "${YELLOW}Pulling website${DEFAULT}"
 git pull --rebase # website
