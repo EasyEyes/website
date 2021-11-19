ba500a280499e87bd54dbc1cc13a1bc22f3f25d2
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2021-11-19 15:37:54 -0500

    website: new direct page

--- docs/redirect/redirect.js
+++ docs/redirect/redirect.js
@@ -3,6 +3,26 @@ const hrefSplit = window.location.href.split("/redirect/");
 const urlSearchParams = new URLSearchParams(window.location.href);
 const redirectTarget = urlSearchParams.get("state");
 
+// var dots = window.setInterval(function () {
+//   var wait = document.getElementById("wait");
+//   switch ((wait.innerHTML.match(/\./g) || []).length) {
+//     case 0:
+//       wait.innerHTML = ".&nbsp;&nbsp;";
+//       break;
+//     case 1:
+//       wait.innerHTML = "..&nbsp;";
+//       break;
+//     case 2:
+//       wait.innerHTML = "...";
+//       break;
+//     case 3:
+//       wait.innerHTML = "&nbsp;&nbsp;&nbsp;";
+//       break;
+//     default:
+//       break;
+//   }
+// }, 500);
+
 const sleep = (time) => {
   return new Promise((resolve) => setTimeout(resolve, time));
 };
