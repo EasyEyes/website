45fa4e72ef0c142209451915c7c804405e9d64c5
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2022-05-14 22:35:20 -0400

    for threshold: debug update

--- scripts/git-pull.sh
+++ scripts/git-pull.sh
@@ -3,12 +3,12 @@
 DEFAULT=$(tput setaf 7)
 YELLOW=$(tput setaf 3)
 
-cd docs/threshold/threshold
-# cd docs/threshold/threshold/psychojs
+# cd docs/threshold/threshold
+cd docs/threshold/threshold/psychojs
 
-# echo "${YELLOW}Pulling psychojs${DEFAULT}"
-# git pull --rebase # psychojs
-# cd ..
+echo "${YELLOW}Pulling psychojs${DEFAULT}"
+git pull --rebase # psychojs
+cd ..
 
 echo "${YELLOW}Pulling threshold${DEFAULT}"
 git pull --rebase # threshold
