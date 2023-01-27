4542f06fd054a71bbca352d0317b45b700a8a1eb
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2023-01-26 20:40:10 -0800

    for threshold-scientist: try netlify functions

---
+++ .netlify/functions/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
---
+++ .netlify/functions/prolific/index.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+
+exports.handler = async (event, context) => {
+  let data;
+
+  // 'users/me/' or 'studies/'
+  const task = event.path
+    .replace("/.netlify/functions/prolific/", "")
+    .replace(/\//gim, "");
+
+  try {
+    const response = await fetch(`https://api.prolific.co/api/v1/${task}`);
+    data = await response.json();
+  } catch (error) {
+    data = {
+      status: "ERROR",
+      error: error.message,
+    };
+  }
+
+  return data;
+};
