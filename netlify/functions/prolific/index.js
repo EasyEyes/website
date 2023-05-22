5331c7374ec7c6174ab2f9addefad5e8d179f2a4
ritikamotwani <ritika.motwani0712@gmail.com>
2023-05-22 10:35:58 -0700

    for threshold-scientist: Remove debugging statements for prolific downloads

--- netlify/functions/prolific/index.js
+++ netlify/functions/prolific/index.js
@@ -108,8 +108,7 @@ exports.handler = async (event, context) => {
         },
       });
 
-      data = response;
-      console.log(data, "ritika");
+      data = await response.text();
       statusCode = 200;
     } catch (error) {
       console.error("ERROR", error);
